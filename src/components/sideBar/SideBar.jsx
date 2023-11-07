a3c3bc4ba39f3180688dd1f02fb8760ae6a2d11a
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-11-07 11:48:30 +0200

    Add Side bar links

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -4,54 +4,59 @@ import { Link } from 'react-router-dom';
 import { AiFillHome } from 'react-icons/ai';
 import { SlCalender } from 'react-icons/sl';
 import {
-  BsFillTrophyFill,
   BsFileEarmarkCheckFill,
   BsFillCalendarDateFill,
+  BsCheckAll,
 } from 'react-icons/bs';
-import { AiFillSchedule, AiFillQuestionCircle } from 'react-icons/ai';
+import { AiFillQuestionCircle } from 'react-icons/ai';
 import { FaUsers, FaUsersCog, FaUserCog } from 'react-icons/fa';
 import { FcInvite } from 'react-icons/fc';
 import { MdAnnouncement, MdReportProblem } from 'react-icons/md';
 import { FcStatistics } from 'react-icons/fc';
 import { TbAdjustmentsCheck } from 'react-icons/tb';
-import { BiSolidLogOut } from 'react-icons/bi';
+import { BiSolidLogOut, BiBookOpen, BiSolidBookOpen } from 'react-icons/bi';
+import { TfiAnnouncement } from 'react-icons/tfi';
 
 const SideBar = () => {
   return (
     <nav className={classes.sideBarContainer}>
-      <h3 className={classes.logo}>CPL</h3>
+      <h3 className={classes.logo}>Bradey High</h3>
       <div className={classes.linksContainer}>
         <div className={classes.homeLinkCont}>
           <p className={classes.linkHeading}>HOME</p>
           <Link className={classes.homeLink}>
             <AiFillHome />
-            <p className={classes.homeText}>Overview</p>
+            <p className={classes.homeText}>Dashboard</p>
           </Link>
         </div>
         <div className={classes.homeLinkCont}>
-          <p className={classes.linkHeading}>MATCHES</p>
+          <p className={classes.linkHeading}>REPORTS</p>
           <Link className={classes.homeLink}>
             <SlCalender />
-            <p className={classes.homeText}>Fixtures</p>
+            <p className={classes.homeText}>Exam Schedule</p>
           </Link>
           <Link className={classes.homeLink}>
-            <AiFillSchedule />
-            <p className={classes.homeText}>Results</p>
+            <BsCheckAll />
+            <p className={classes.homeText}>Marks</p>
           </Link>
           <Link className={classes.homeLink}>
-            <BsFillTrophyFill />
-            <p className={classes.homeText}>Standings</p>
+            <BiBookOpen />
+            <p className={classes.homeText}>End of Month Report</p>
+          </Link>
+          <Link className={classes.homeLink}>
+            <BiSolidBookOpen />
+            <p className={classes.homeText}>End of Term Report</p>
           </Link>
         </div>
         <div className={classes.homeLinkCont}>
-          <p className={classes.linkHeading}>CLUBS</p>
+          <p className={classes.linkHeading}>STUDENTS</p>
           <Link className={classes.homeLink}>
             <FaUsers />
-            <p className={classes.homeText}>Teams</p>
+            <p className={classes.homeText}>My Class</p>
           </Link>
           <Link className={classes.homeLink}>
             <FaUsersCog />
-            <p className={classes.homeText}>Team Sheets</p>
+            <p className={classes.homeText}>Other Classes</p>
           </Link>
         </div>
         <div className={classes.homeLinkCont}>
@@ -70,56 +75,60 @@ const SideBar = () => {
           </Link>
         </div>
         <div className={classes.homeLinkCont}>
-          <p className={classes.linkHeading}>TEAM ADMIN</p>
+          <p className={classes.linkHeading}>SCHOOL ADMIN</p>
           <Link className={classes.homeLink}>
             <BsFileEarmarkCheckFill />
-            <p className={classes.homeText}>Submit Team Sheet</p>
+            <p className={classes.homeText}>Add Teacher</p>
           </Link>
           <Link className={classes.homeLink}>
             <FcInvite />
-            <p className={classes.homeText}>Invite Team Members</p>
+            <p className={classes.homeText}>Add Student</p>
           </Link>
           <Link className={classes.homeLink}>
             <FaUsersCog />
-            <p className={classes.homeText}>Manage Team</p>
+            <p className={classes.homeText}>Manage Teachers</p>
           </Link>
           <Link className={classes.homeLink}>
             <TbAdjustmentsCheck />
-            <p className={classes.homeText}>Manage Fixtures</p>
+            <p className={classes.homeText}>Manage Students</p>
+          </Link>
+          <Link className={classes.homeLink}>
+            <TfiAnnouncement />
+            <p className={classes.homeText}>Exam Timetable</p>
+          </Link>
+          <Link className={classes.homeLink}>
+            <TfiAnnouncement />
+            <p className={classes.homeText}>Make An Announcement</p>
           </Link>
         </div>
         <div className={classes.homeLinkCont}>
-          <p className={classes.linkHeading}>LEAGUE ADMIN</p>
+          <p className={classes.linkHeading}>TEACHER</p>
           <Link className={classes.homeLink}>
             <BsFileEarmarkCheckFill />
-            <p className={classes.homeText}>Approve Team Sheet</p>
+            <p className={classes.homeText}>Schedule Monthly Test</p>
           </Link>
           <Link className={classes.homeLink}>
             <FcInvite />
-            <p className={classes.homeText}>Invite Team Captains</p>
+            <p className={classes.homeText}>Enter Marks</p>
           </Link>
           <Link className={classes.homeLink}>
             <BsFillCalendarDateFill />
-            <p className={classes.homeText}>Manage Dates</p>
+            <p className={classes.homeText}>Manage Tests</p>
           </Link>
           <Link className={classes.homeLink}>
             <FaUsersCog />
-            <p className={classes.homeText}>Manage Users</p>
-          </Link>
-          <Link className={classes.homeLink}>
-            <TbAdjustmentsCheck />
-            <p className={classes.homeText}>Manage Fixtures</p>
+            <p className={classes.homeText}>Manage Term Exams</p>
           </Link>
         </div>
         <div className={classes.homeLinkCont}>
           <p className={classes.linkHeading}>SYSTEM ADMIN</p>
           <Link className={classes.homeLink}>
             <FcInvite />
-            <p className={classes.homeText}>Invite League Admins</p>
+            <p className={classes.homeText}>Add School</p>
           </Link>
           <Link className={classes.homeLink}>
             <FaUsersCog />
-            <p className={classes.homeText}>Manage Teams</p>
+            <p className={classes.homeText}>Manage Schools</p>
           </Link>
           <Link className={classes.homeLink}>
             <FaUsersCog />
@@ -134,7 +143,7 @@ const SideBar = () => {
           </Link>
           <Link className={classes.homeLink}>
             <FcStatistics />
-            <p className={classes.homeText}>Game Statistics</p>
+            <p className={classes.homeText}>Account Settings</p>
           </Link>
           <Link className={classes.homeLink}>
             <BiSolidLogOut />
